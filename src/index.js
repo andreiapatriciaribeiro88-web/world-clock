a26b256fa8784f3c8251decc087bc5bb3ab6b54f
andreiapatriciaribeiro88-web <andreiapatriciaribeiro88@gmail.com>
2025-09-30 15:01:44 +0100

    Add updateTime function to display time for all cities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
+function updateTime() {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
 
+  sydneyDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[]</small>"
+  );
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[]</small>");
+
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgDateElement = johannesburgElement.querySelector(".date");
+  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+  let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+  johannesburgDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  johannesburgTimeElement.innerHTML = johannesburgTime.format(
+    "h:mm:ss [<small>]A[]</small>"
+  );
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML =
+    londonTime.format("h:mm:ss") + ` <small>${londonTime.format("A")}</small>`;
+
+  let saoPauloElement = document.querySelector("#sao-paulo");
+  let saoPauloDateElement = saoPauloElement.querySelector(".date");
+  let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+  let saoPauloTime = moment().tz("America/Sao_Paulo");
+
+  saoPauloDateElement.innerHTML = saoPauloTime.format("MMMM Do YYYY");
+  saoPauloTimeElement.innerHTML =
+    saoPauloTime.format("h:mm:ss") +
+    ` <small>${saoPauloTime.format("A")}</small>`;
+
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML =
+    losAngelesTime.format("h:mm:ss") +
+    ` <small>${losAngelesTime.format("A")}</small>`;
+
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML =
+    newYorkTime.format("h:mm:ss") +
+    ` <small>${newYorkTime.format("A")}</small>`;
+}
+updateTime();
+setInterval(updateTime, 1000);
