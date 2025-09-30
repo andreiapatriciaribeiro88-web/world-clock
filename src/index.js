8cecbfc0055edc1f1ef980cbe50d54a4161921c0
andreiapatriciaribeiro88-web <andreiapatriciaribeiro88@gmail.com>
2025-09-30 16:15:04 +0100

    added current time zone

--- src/index.js
+++ src/index.js
@@ -72,6 +72,9 @@ function updateCity(event) {
   if (cityTimeZone === "all") {
     return;
   }
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
